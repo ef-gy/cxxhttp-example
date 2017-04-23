23972098a8ea4d219f05c14e4b9a324793f2e91b
Magnus Deininger <magnus@ef.gy>
2017-04-23 15:34:35 +0000

    the cxxhttp namespace has been flattened further a bit.

--- src/example.cpp
+++ src/example.cpp
@@ -3,17 +3,18 @@
 using namespace cxxhttp;
 
 template <class transport>
-static bool hello(typename net::http::server<transport>::session &session, std::smatch &) {
+static bool hello(typename http::server<transport>::session &session,
+                  std::smatch &) {
   session.reply(200, "Hello World!");
   return true;
 }
 
 namespace tcp {
-  static httpd::servlet<transport::tcp> hello("/", ::hello<transport::tcp>);
+static httpd::servlet<transport::tcp> hello("/", ::hello<transport::tcp>);
 }
 
 namespace unix {
-  static httpd::servlet<transport::unix> hello("/", ::hello<transport::unix>);
+static httpd::servlet<transport::unix> hello("/", ::hello<transport::unix>);
 }
 
 int main(int argc, char *argv[]) { return cxxhttp::main(argc, argv); }
